a47f02aee098501a17d3536e516a46327115d2de
Jean Paul Torre <jean.paul.ruben@gmail.com>
2019-10-09 03:03:13 +0800

    Remove unused imports

--- src/main/java/com/fcgl/madrid/search/util/CategoryService.java
+++ src/main/java/com/fcgl/madrid/search/util/CategoryService.java
@@ -4,22 +4,13 @@ import com.fcgl.madrid.search.dataModel.Category;
 import com.fcgl.madrid.search.payload.InternalStatus;
 import com.fcgl.madrid.search.payload.response.Response;
 import com.fcgl.madrid.search.repository.CategoryRepository;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionSystemException;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class CategoryService {
