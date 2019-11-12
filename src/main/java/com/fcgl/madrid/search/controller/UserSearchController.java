7d42b96690b1cc2abaaa310553b9a721f5b4a53b
kartik <kartik.aggarwal@stu.bmcc.cuny.edu>
2019-11-12 14:50:11 -0500

    Added paginated GET endpoint for UserSearch

--- src/main/java/com/fcgl/madrid/search/controller/UserSearchController.java
+++ src/main/java/com/fcgl/madrid/search/controller/UserSearchController.java
@@ -25,7 +25,7 @@ public class UserSearchController {
     /**
      * We want to keep track of the searches a user has made
      * This endpoint simply adds an entry to the UserSearch database table based on the search a user has made
-     *
+     * <p>
      * TODO: Based on what we find out on elastic search. We might have to make an api request to elastic search from here as well.
      */
     @PostMapping(path = "/query")
@@ -33,8 +33,28 @@ public class UserSearchController {
         return userSearchService.addQueryToTable(request);
     }
 
-    @GetMapping(path = "/query") //TODO returns a 500 not sure why
-    public ResponseEntity<Response<SearchHistoryResponse>> getQueriesForUser(@Valid UserId request) {
-            return userSearchService.searchByUserId(request);
+    /**
+     * This endpoint returns a paginated list of queries that a user has made.
+     *
+     * @param id       specify user id to be queried
+     * @param pageSize Optional parameter: specify number of queries per page
+     * @param page     Optional parameter: specify page number of results
+     * @return list of results
+     */
+    @GetMapping(path = "query") //expect page index starting from 1, so subtract one for querying purposes
+    public ResponseEntity<Response<SearchHistoryResponse>> getRecentQueriesForUser(@Valid UserId id,
+                                                                                   @RequestParam(required = false) Integer pageSize,
+                                                                                   @RequestParam(required = false) Integer page) {
+        // handle default page size value
+        if (pageSize == null) {
+            pageSize = 10;
+        }
+        // handle default page value
+        if (page == null) {
+            page = 0;
+        } else {
+            page = page - 1;
+        }
+        return userSearchService.searchByUserIdRecent(id, pageSize, page);
     }
 }
--- src/main/java/com/fcgl/madrid/search/service/UserSearchService.java
+++ src/main/java/com/fcgl/madrid/search/service/UserSearchService.java
@@ -4,21 +4,21 @@ import com.fcgl.madrid.search.payload.request.UserId;
 import com.fcgl.madrid.search.dataModel.UserSearch;
 import com.fcgl.madrid.search.payload.InternalStatus;
 import com.fcgl.madrid.search.payload.request.UserSearchRequest;
+import com.fcgl.madrid.search.payload.response.PaginatedResponse;
 import com.fcgl.madrid.search.payload.response.Response;
 import com.fcgl.madrid.search.payload.response.SearchHistoryResponse;
 import com.fcgl.madrid.search.repository.UserSearchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class UserSearchService {
-    //TODO: Add necessary functions
 
     private UserSearchRepository userSearchRepo;
 
@@ -27,6 +27,12 @@ public class UserSearchService {
         this.userSearchRepo = userSearchRepo;
     }
 
+    /**
+     * This method adds the given query to the UserSearch table.
+     *
+     * @param request UserSearchRequest
+     * @return Status
+     */
     public ResponseEntity<Response<String>> addQueryToTable(UserSearchRequest request) {
         Long userId = request.getUserId();
         String query = request.getQuery();
@@ -54,9 +60,23 @@ public class UserSearchService {
 
     }
 
-    public ResponseEntity<Response<SearchHistoryResponse>> searchByUserId(UserId userId) {
-        List<UserSearch> searchHistory = userSearchRepo.findByUserId(userId);
-        Response response = new Response<SearchHistoryResponse>(InternalStatus.OK, new SearchHistoryResponse(searchHistory));
+    /**
+     * This method returns queries in a paginated format made by a user.
+     *
+     * @param id       User id
+     * @param pageSize Number of entries per page
+     * @param page     Which page number
+     * @return response
+     */
+    public ResponseEntity<Response<SearchHistoryResponse>> searchByUserIdRecent(UserId id, int pageSize, int page) {
+        // Paginate List
+        Pageable pageableRequest = PageRequest.of(page, pageSize);
+        List<UserSearch> searchHistory = userSearchRepo.findByUserIdOrderByAddedOnDesc(id, pageableRequest);
+        // Get total number of records
+        int records = userSearchRepo.countByUserId(id);
+        // Format Response
+        PaginatedResponse paginatedResponse = new PaginatedResponse<>(new SearchHistoryResponse(searchHistory), pageSize, page + 1, records); // to user we want to show non-zero index
+        Response response = new Response<>(InternalStatus.OK, paginatedResponse);
         return new ResponseEntity<Response<SearchHistoryResponse>>(response, HttpStatus.OK);
     }
 }
