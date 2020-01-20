87f1ade2abe675cba00c8d3365b6e91db3c66cc1
kartik <kartik.aggarwal@stu.bmcc.cuny.edu>
2020-01-19 23:03:50 -0500

    added deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -60,7 +60,7 @@ make_task_def(){
 
 push_ecr_image(){
 	eval $(aws ecr get-login --region us-east-1 --no-include-email)
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
+	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/search:$CIRCLE_SHA1
 }
 
 register_definition() {
