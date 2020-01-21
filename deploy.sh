6b6af977b44d7e76da0f3872c9e1de3f96a6bda3
kartik <kartik.aggarwal@stu.bmcc.cuny.edu>
2020-01-20 20:58:57 -0500

    test change

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ deploy_cluster() {
 
     make_task_def
     register_definition
-    if [[ $(aws ecs update-service --cluster search-cluster --service search-service --task-definition $revision | \
+    if [[ $(aws ecs update-service --cluster search-cluster --service search-service --requires-compatibilities "FARGATE" --task-definition $revision | \
                    $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
@@ -24,7 +24,7 @@ deploy_cluster() {
     # wait for older revisions to disappear
     # not really necessary, but nice for demos
     for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster search-cluster --services search-service | \
+        if stale=$(aws ecs describe-services --cluster search-cluster --requires-compatibilities "FARGATE" --services search-service | \
                        $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
             echo "Waiting for stale deployments:"
             echo "$stale"
